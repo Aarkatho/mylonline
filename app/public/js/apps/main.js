0bfefeb4b1a849e2bf8b78319c84e40903683d1c
Narkatho <narkatho@gmail.com>
2016-08-18 17:49:16 -0300

    180820161749

--- app/public/js/apps/main.js
+++ app/public/js/apps/main.js
@@ -1,3 +1,13 @@
-define(['router'], function (router) {
+define(['backbone', 'router'], function (BB, router) {
     console.log('--- APP PRINCIPAL CARGADA ---');
+
+    var useToken = function (token) {
+        BB.$.ajaxSetup({
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-Access-Token', token);
+            }
+        });
+    };
+
+    useToken();
 });
